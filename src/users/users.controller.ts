4cdba84d1c43265d5febc15a5af5920e7a3e4eaa
Victory Mwendwa <mwendwavictory29@gmail.com>
2026-06-02 14:41:06 +0300

    A little bit of dependency injection and updated the routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,16 +1,25 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
 
+    constructor(private readonly usersService: UsersService){}
+    /* 
+    creates an instance of the usersService. Again, if it was created elsewhere, another instance, it pulls it in coz nestjs identifies that it's a singleton. if we were to create it without the nestjs benefit, we would have:
+    -> const usersService = new UsersService()
+    but nest is handling this for us and actually doing more than that. It's saying "Hey, if we created this elsewhere, it's going to find it and pull it in"
+    */ 
+    
+
     @Get( ) //GET /users (or users?role=value) -> query parameter (optional)
     findAll(@Query('role')role?:'INTERN' | 'ENGINEER' | 'ADMIN') {
-        return[]
+        return this.usersService.findAll(role)
     }
 
     @Get(':id') // GET /users/:id
     findOne(@Param('id')id: string) {
-    return {id}
+    return this.usersService.findOne(+id) // Unary plus(+) -> An easy way to convert something into a number (the id is passed in as a string and needs to be converted into a number)
     }
 
     /**
@@ -22,20 +31,21 @@ export class UsersController {
      */
     
     @Post() //POST /users
-    create(@Body()user: {}) {
-        return user
+    create(@Body()user: {name: string, email: string, role: 'INTERN' | 'ENGINEER' | 'ADMIN'}) {
+        return this.usersService.create(user)
 
     }
 
     @Patch(':id') // PATCH /users/:id
-    update(@Param('id')id: string, @Body() userUpdate: {}) {
-    return {id, ...userUpdate}
+    update(@Param('id')id: string, @Body() userUpdate: {name?: string, email?: string, role?: 'INTERN' | 'ENGINEER' | 'ADMIN'}) {
+    return this.usersService.update(+id, userUpdate)
     }
 
     @Delete(':id') // DELETE /users/:id
-    elete(@Param('id')id: string) {
-    return {id}
+    delete(@Param('id')id: string) {
+    return this.usersService.delete(+id)
     }
 
 
 }
+
